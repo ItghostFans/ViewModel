7b720b9839aa43297dee2829b2a780ec4d570fae
ItghostFans <ItghostFans@gmail.com>
2024-07-05 00:38:09 +0800

    feat (demo): 完成Demo。

--- Example/MVVM/Test/TestTableController.m
+++ Example/MVVM/Test/TestTableController.m
@@ -9,12 +9,16 @@
 #import "TestTableController.h"
 #import "TestTableControllerViewModel.h"
 
-#import "TableViewModel.h"
-
 #import <Masonry/Masonry.h>
+#import <ReactiveObjC/ReactiveObjC.h>
+
+#import "TableViewModel.h"
+#import "TestCellViewModel.h"
+#import "TestSectionViewModel.h"
 
 @interface TestTableController ()
-// TODO: 添加需要的View，建议使用懒加载
+@property (weak, nonatomic) UIButton *addSectionButton;
+@property (weak, nonatomic) UIButton *addRowButton;
 @end
 
 @implementation TestTableController
@@ -26,9 +30,35 @@
     self.view.backgroundColor = UIColor.whiteColor;
     [self.view addSubview:self.tableView];
     [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self.view);
+        make.top.equalTo(self.addSectionButton.mas_bottom);
+        make.leading.trailing.bottom.equalTo(self.view);
     }];
     self.viewModel.tableViewModel.tableView = self.tableView;
+    
+    @weakify(self);
+    self.addSectionButton.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
+        @strongify(self);
+        [self.viewModel.tableViewModel.tableView performBatchUpdates:^{
+            @strongify(self);
+            TestSectionViewModel *sectionViewModel = TestSectionViewModel.new;
+            [sectionViewModel addViewModel:TestCellViewModel.new];
+            [self.viewModel.tableViewModel.sectionViewModels addViewModel:sectionViewModel];
+        } completion:^(BOOL finished) {
+        }];
+        return [RACSignal return:nil];
+    }];
+    self.addRowButton.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
+        @strongify(self);
+        [self.viewModel.tableViewModel.tableView performBatchUpdates:^{
+            @strongify(self);
+            SectionViewModel *sectionViewModel = self.viewModel.tableViewModel.sectionViewModels.viewModels.firstObject;
+            if (sectionViewModel) {
+                [sectionViewModel addViewModel:TestCellViewModel.new];
+            }
+        } completion:^(BOOL finished) {
+        }];
+        return [RACSignal return:nil];
+    }];
 }
 
 #pragma mark - Public
@@ -39,6 +69,34 @@
 
 #pragma mark - Getter
 
-// TODO: 添加需要的View，建议使用懒加载
+- (UIButton *)addSectionButton {
+    if (!_addSectionButton) {
+        UIButton *addSectionButton = UIButton.new;
+        [self.view addSubview:addSectionButton];
+        _addSectionButton = addSectionButton;
+        [_addSectionButton setTitle:@"Add Section" forState:(UIControlStateNormal)];
+        [_addSectionButton setTitleColor:UIColor.blackColor forState:(UIControlStateNormal)];
+        [_addSectionButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+            make.leading.equalTo(self.view);
+        }];
+    }
+    return _addSectionButton;
+}
+
+- (UIButton *)addRowButton {
+    if (!_addRowButton) {
+        UIButton *addRowButton = UIButton.new;
+        [self.view addSubview:addRowButton];
+        _addRowButton = addRowButton;
+        [_addRowButton setTitle:@"Add Row" forState:(UIControlStateNormal)];
+        [_addRowButton setTitleColor:UIColor.blackColor forState:(UIControlStateNormal)];
+        [_addRowButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.view.mas_safeAreaLayoutGuideTop);
+            make.leading.equalTo(self.addSectionButton.mas_trailing);
+        }];
+    }
+    return _addRowButton;
+}
 
 @end
