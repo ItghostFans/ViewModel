c38de8e7c264b180a627f01920db877cbd703444
ItghostFans <ItghostFans@gmail.com>
2024-09-08 00:29:30 +0800

    chore (template): 模板更新。

--- Resources/Templates/CollectionViewModelCell.xctemplate/CollectionViewModelCellObjective-C/___VARIABLE_productName___CollectionViewModelCell.h
+++ Resources/Templates/CollectionViewModelCell.xctemplate/CollectionViewModelCellObjective-C/___VARIABLE_productName___CollectionViewModelCell.h
@@ -4,7 +4,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class ___VARIABLE_productName___CollectionCellViewModel;
+
 @interface ___VARIABLE_productName___CollectionViewModelCell : CollectionViewModelCell
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincompatible-property-type"
+#pragma clang diagnostic ignored "-Wobjc-property-synthesis"
+@property (weak, nonatomic, nullable) ___VARIABLE_productName___CollectionCellViewModel *viewModel;
+#pragma clang diagnostic pop
 
 @end
 
--- Resources/Templates/TableViewModelCell.xctemplate/TableViewModelCellObjective-C/___VARIABLE_productName___TableViewModelCell.h
+++ Resources/Templates/TableViewModelCell.xctemplate/TableViewModelCellObjective-C/___VARIABLE_productName___TableViewModelCell.h
@@ -4,7 +4,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class ___VARIABLE_productName___TableCellViewModel;
+
 @interface ___VARIABLE_productName___TableViewModelCell : TableViewModelCell
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincompatible-property-type"
+#pragma clang diagnostic ignored "-Wobjc-property-synthesis"
+@property (weak, nonatomic, nullable) ___VARIABLE_productName___TableCellViewModel *viewModel;
+#pragma clang diagnostic pop
 
 @end
 
