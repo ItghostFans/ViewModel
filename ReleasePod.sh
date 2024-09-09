ffe5d434affa310c7d5dd3d85505ba1a45bdd9f0
ItghostFans <ItghostFans@gmail.com>
2024-09-10 01:11:50 +0800

    1.0.0

--- ReleasePod.sh
+++ ReleasePod.sh
@@ -8,6 +8,11 @@ podspec=`ls *.podspec`
 podName=${podspec%.*}
 
 sed -i '' "s/\'[0-9]*\.[0-9]*\.[0-9]*.*\'/\'${tag}\'/g" "$podName.podspec"
+
+git add *
+git commit -m $tag
+git push -u origin main
+
 git tag $tag
 git push origin $tag
 git push origin --tags
