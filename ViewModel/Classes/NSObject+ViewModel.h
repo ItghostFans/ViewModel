ffe9ba4c3f34c28f295f1f9ac2b533ceadac93db
ItghostFans <ItghostFans@gmail.com>
2026-01-20 11:44:43 +0800

    feat (Array Kvo): 数组KVO。

---
+++ ViewModel/Classes/NSObject+ViewModel.h
@@ -0,0 +1,42 @@
+//
+//  NSObject+ViewModel.h
+//  Pods
+//
+//  Created by ItghostFan on 2026/1/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class RACDisposable;
+
+@interface NSObject (ViewModel)
+
+- (RACDisposable *)vm_arrayChangesForKeyPath:(NSString *)keyPath
+                                      object:(NSObject *)object
+                                    observer:(__weak NSObject *)observer
+                                     context:(__weak NSObject * _Nullable)context
+                                  settingSel:(SEL)settingSel
+                                insertionSel:(SEL)insertionSel
+                                  removalSel:(SEL)removalSel
+                              replacementSel:(SEL)replacementSel;
+
+- (void)vm_settingSel:(NSArray *)elements
+              context:(__weak NSObject * _Nullable)context;
+
+- (void)vm_insertionSel:(NSArray *)elements
+                context:(__weak NSObject * _Nullable)context
+                indexes:(NSMutableArray<__kindof NSNumber *> *)indexes;
+
+- (void)vm_removalSel:(NSArray *)elements
+              context:(__weak NSObject * _Nullable)context
+              indexes:(NSMutableArray<__kindof NSNumber *> *)indexes;
+
+- (void)vm_replacementSel:(NSArray *)elements
+                  context:(__weak NSObject * _Nullable)context
+                  indexes:(NSMutableArray<__kindof NSNumber *> *)indexes;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ViewModel/Classes/NSObject+ViewModel.m
@@ -0,0 +1,90 @@
+//
+//  NSObject+ViewModel.m
+//  Pods
+//
+//  Created by ItghostFan on 2026/1/13.
+//
+
+#import "NSObject+ViewModel.h"
+
+#import <ReactiveObjC/ReactiveObjC.h>
+
+@implementation NSObject (ViewModel)
+
+- (RACDisposable *)vm_arrayChangesForKeyPath:(NSString *)keyPath
+                                      object:(NSObject *)object
+                                    observer:(__weak NSObject *)observer
+                                     context:(__weak NSObject * _Nullable)context
+                                  settingSel:(SEL)settingSel
+                                insertionSel:(SEL)insertionSel
+                                  removalSel:(SEL)removalSel
+                              replacementSel:(SEL)replacementSel {
+    return [[[object rac_valuesAndChangesForKeyPath:keyPath
+                                   options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld | NSKeyValueObservingOptionInitial
+                                      observer:observer] takeUntil:observer.rac_willDeallocSignal] subscribeNext:^(RACTwoTuple<id,NSDictionary *> * _Nullable x) {
+        RACTupleUnpack(id object OS_UNUSED, NSDictionary *change) = x;
+        NSKeyValueChange valueChange = [change[NSKeyValueChangeKindKey] unsignedIntegerValue];
+        NSMutableArray<__kindof NSNumber *> *indexes = NSMutableArray.new;
+        [(NSIndexSet *)change[NSKeyValueChangeIndexesKey] enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
+            [indexes addObject:@(idx)];
+        }];
+        NSArray *news = change[NSKeyValueChangeNewKey];
+        if ([news isKindOfClass:NSNull.class]) {
+            news = nil;
+        }
+        NSArray *olds = change[NSKeyValueChangeOldKey];
+        if ([olds isKindOfClass:NSNull.class]) {
+            olds = nil;
+        }
+        NSInteger paramIndex = 2;
+        NSInvocation *invocation = nil;
+        switch (valueChange) {
+            case NSKeyValueChangeSetting: {
+                invocation = [self vm_invocationOfObject:observer selector:settingSel];
+                [invocation setArgument:(void *)&news atIndex:paramIndex++];
+                [invocation setArgument:(void *)&context atIndex:paramIndex++];
+                break;
+            }
+            case NSKeyValueChangeInsertion: {
+                invocation = [self vm_invocationOfObject:observer selector:insertionSel];
+                [invocation setArgument:(void *)&news atIndex:paramIndex++];
+                [invocation setArgument:(void *)&context atIndex:paramIndex++];
+                [invocation setArgument:(void *)&indexes atIndex:paramIndex++];
+                break;
+            }
+            case NSKeyValueChangeRemoval: {
+                invocation = [self vm_invocationOfObject:observer selector:removalSel];
+                [invocation setArgument:(void *)&olds atIndex:paramIndex++];
+                [invocation setArgument:(void *)&context atIndex:paramIndex++];
+                [invocation setArgument:(void *)&indexes atIndex:paramIndex++];
+                break;
+            }
+            case NSKeyValueChangeReplacement: {
+                invocation = [self vm_invocationOfObject:observer selector:replacementSel];
+                [invocation setArgument:(void *)&news atIndex:paramIndex++];
+                [invocation setArgument:(void *)&context atIndex:paramIndex++];
+                [invocation setArgument:(void *)&indexes atIndex:paramIndex++];
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+        [invocation invoke];
+        NSLog(@"Fuck Invoke");
+    } error:^(NSError * _Nullable error) {
+        NSLog(@"Fuck Error");
+    } completed:^{
+        NSLog(@"Fuck Completion");
+    }];
+}
+
+- (nullable NSInvocation *)vm_invocationOfObject:(NSObject *)object selector:(SEL)selector {
+    NSMethodSignature *methodSignature = [object.class instanceMethodSignatureForSelector:selector];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+    invocation.target = object;
+    invocation.selector = selector;
+    return invocation;
+}
+
+@end
